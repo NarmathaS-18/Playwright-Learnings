68518a42640166636ae5569edfb2ac8809aa6c68
Narmatha <narmathasundaram17@gmail.com>
2026-07-24 06:28:09 +0530

    Initial commit

---
+++ Chapter_02/switch_statement.js
@@ -0,0 +1,23 @@
+let day = 3;
+//mon =1, tue = 2,..
+
+switch (day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    // Add more cases as needed
+    default:
+        console.log("Invalid day");
+}
