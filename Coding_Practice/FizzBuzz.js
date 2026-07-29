936bb1b764dad0bf7c730c2e184a8709bc76aa5e
Narmatha <narmathasundaram17@gmail.com>
2026-07-29 17:02:47 +0530

    Add FizzBuzz

---
+++ Coding_Practice/FizzBuzz.js
@@ -0,0 +1,18 @@
+const data = require('fs').readFileSync(0, 'utf8').trim();
+let n = Number(data);
+
+for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0)
+         {
+        console.log("FizzBuzz");
+    } 
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    } 
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    } 
+    else {
+        console.log(i);
+    }
+}
