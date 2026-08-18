00a0c2b223925e5ed456e47be2f4b44877306484
Narmatha <narmathasundaram17@gmail.com>
2026-08-18 19:06:53 +0530

    coding practice

--- Coding_Practice/PrimeNumberChecker.js
+++ Coding_Practice/PrimeNumberChecker.js
@@ -1,10 +1,18 @@
 const num = 17;
 let found = false;
 
-for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-        found = true;
-        break;
+if (num < 2) {
+    found = true; // treat 0, 1, negatives as "not prime"
+} else if (num === 2) {
+    found = false; // 2 is prime
+} else if (num % 2 === 0) {
+    found = true; // other even numbers aren't prime
+} else {
+    for (let i = 3; i * i <= num; i += 2) {
+        if (num % i === 0) {
+            found = true;
+            break;
+        }
     }
 }
 
